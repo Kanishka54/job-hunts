e88dc18a94387dc844896908be33b141e13ee8ba
Kanishka Bhutani <kanishkabhutani5@gmail.com>
2025-08-20 00:57:13 +0530

    first-commit

--- server/app.js
+++ server/app.js
@@ -30,6 +30,10 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
