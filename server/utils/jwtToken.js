e734eb28d2eb3a1d2f7605b61ffc9cb7586312f5
Kanishka Bhutani <kanishkabhutani5@gmail.com>
2025-08-21 01:10:27 +0530

    first-commit

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -1,16 +1,22 @@
 export const sendToken = (user, statusCode, res, message) => {
-    const token = user.getJWTToken();
-    const options = {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true, // Set httpOnly to true
-    };
-  
-    res.status(statusCode).cookie("token", token, options).json({
+  const token = user.getJWTToken();
+
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // ✅ true only in prod
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // ✅ works locally + prod
+  };
+
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({
       success: true,
       user,
       message,
       token,
     });
-  };
+};
