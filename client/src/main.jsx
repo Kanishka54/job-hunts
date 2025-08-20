e4a6140ebc5d1775886951ff81a99a220f7c1b01
Kanishka Bhutani <kanishkabhutani5@gmail.com>
2025-08-20 18:29:00 +0530

    first-commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import axios from "axios";
 
 export const Context = createContext({
   isAuthorized: false,
@@ -9,6 +10,33 @@ export const Context = createContext({
 const AppWrapper = () => {
   const [isAuthorized, setIsAuthorized] = useState(false);
   const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://job-hunts-1.onrender.com/api/v1/user/getuser",
+          {
+            withCredentials: true,
+          }
+        );
+        setIsAuthorized(true);
+        setUser(data.user);
+      } catch (error) {
+        setIsAuthorized(false);
+        setUser({});
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>; // Or your loading component
+  }
 
   return (
     <Context.Provider
